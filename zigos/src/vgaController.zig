c5c6d6d2e61866f290b75abe050c74f217d9952c
Inge A. Suhr <ofelas@0x2511.com>
2016-10-29 22:32:40 +0200

    Rename ioutils to vgaController and cleanup

---
+++ zigos/src/vgaController.zig
@@ -0,0 +1,122 @@
+// import unsigned
+//type  PVIDMem* = ptr array[0..65_000, TEntry]
+pub const PVidMem = []TEntry;
+pub const VGAWidth = 80;
+pub const VGAHeight = 25;
+pub const VGA_VIDEO_BASEADDRESS = 0xB8000;
+
+pub struct VGA {
+    const Self = this;
+    width: usize,
+    heigth: usize,
+    baseAddress: usize,
+    vram: PVidMem,
+
+    pub fn init(vga: &Self) {
+        vga.width = VGAWidth;
+        vga.heigth = VGAHeight;
+        vga.baseAddress = usize(VGA_VIDEO_BASEADDRESS);
+        vga.vram.ptr = (&u16)(vga.baseAddress);
+        vga.vram.len = 65000;
+    }
+
+    /// Writes a character at the specified ``pos``.
+    inline fn writeChar(vga: &Self, entry: TEntry, pos: TPos) {
+        const index = usize((vga.width * pos.y) + pos.x);
+        // assert(index < vga.vram.len);
+        vga.vram[index] = entry;
+    }
+
+    /// Clear the screen given the background color
+    pub fn clearScreen(vga: &Self, color: TVGAColor) {
+        const space = makeEntry(' ', makeColor(color, color));
+        {
+            var i = usize(0);
+            while (i <= (vga.width * vga.heigth); i += 1) {
+                vga.vram[i] = space;
+            }
+        }
+    }
+
+    /// Writes a string at the specified ``pos`` with varying colors which, despite
+    /// the name of this function, do not resemble a rainbow.
+    pub fn rainbow(vga: &Self, text: []u8, pos: TPos) {
+        const colorBG = TVGAColor.Black;
+        var colorFG = TVGAColor.Blue;
+
+        for (text) |c, i| {
+            const cc = makeColor(colorBG, colorFG);
+            writeChar(vga, makeEntry(c, cc), TPos{.x = pos.x + i, .y = pos.y});
+            colorFG = colorFG.nextColor();
+        }
+    }
+
+    /// Writes a string at the specified ``pos`` with the specified ``color``.
+    pub fn writeString(vga: &Self, text: []u8, color: TAttribute, pos: TPos) {
+    for (text) |c, i| {
+        //for i in 0 .. text.len-1:
+        writeChar(vga, makeEntry(c, color), TPos{.x = pos.x + i, .y = pos.y});
+    }
+}
+
+}
+
+pub struct TPos {
+    pub x: usize,
+    pub y: usize,
+}
+
+// Some types
+pub const TAttribute = u8;
+pub const TEntry = u16;
+
+pub enum TVGAColor {
+    const Self = this;
+    Black,
+    Blue,
+    Green,
+    Cyan,
+    Red,
+    Magenta,
+    Brown,
+    LightGrey,
+    DarkGrey,
+    LightBlue,
+    LightGreen,
+    LightCyan,
+    LightRed,
+    LightMagenta,
+    Yellow,
+    White,
+
+    pub fn nextColor(color: Self) -> TVGAColor {
+        //proc nextColor(color: TVGAColor, skip: set[TVGAColor]): TVGAColor =
+        var result = color;
+        if ((color == TVGAColor.White) || (color == TVGAColor.Black)) {
+            result = TVGAColor.Blue;
+        } else {
+            result = TVGAColor(u8(color) + 1);
+        }
+        // if result in skip: result = nextColor(result, skip)
+        return result;
+    }
+}
+
+/// Combines a foreground and background color into a ``TAttribute``.
+pub fn makeColor(bg: TVGAColor, fg: TVGAColor) -> TAttribute {
+    return TAttribute(u8(fg) | (u8(bg) << 4));
+}
+
+/// Combines a char and a TAttribute into a format which can be
+/// directly written to the Video memory.
+pub fn makeEntry(c: u8, color: TAttribute) -> TEntry {
+    const c16 = u16(c);
+    const color16 = u16(color);
+    return TEntry(c16 | (color16 << 8));
+}
+
+fn testIOUTILS() {
+    @setFnTest(this, true);
+
+    var c = makeColor(TVGAColor.Black, TVGAColor.Red);
+}
--- zigos/src/zmain.zig
+++ zigos/src/zmain.zig
@@ -1,12 +1,8 @@
-const ioutils = @import("ioutils.zig");
-const makeColor = ioutils.makeColor;
-const writeString = ioutils.writeString;
-const rainbow = ioutils.rainbow;
-const screenClear = ioutils.screenClear;
-const PVidMem = ioutils.PVidMem;
-const TPos = ioutils.TPos;
-const TVGAColor = ioutils.TVGAColor;
-const TEntry = ioutils.TEntry;
+const vgaCtrl = @import("vgaController.zig");
+const makeColor = vgaCtrl.makeColor;
+const TPos = vgaCtrl.TPos;
+const TVGAColor = vgaCtrl.TVGAColor;
+const VGA = vgaCtrl.VGA;
 
 const TMultiboot_header = &u8;
 const MULTIBOOT_MAGIC = u32(0xe85250d6);
@@ -31,18 +27,19 @@ inline fn mkTPos(x: u8, y: u8) -> TPos {
 // }
 
 export nakedcc fn zigmain(mb_header: TMultiboot_header) {
-    var vram: []TEntry = undefined;
-    vram.ptr = (&u16)(usize(0xB8000));
-    vram.len = 65000;
-
-    screenClear(vram, TVGAColor.Green);
+    var vga: VGA = undefined;
     const attr = makeColor(TVGAColor.Black, TVGAColor.White);
-    rainbow(vram, " ---=== Booted by GRUB ===--- ", mkTPos(10, 10));
-    writeString(vram, " Hello from Zig ", attr, mkTPos(10, 11));
-    writeString(vram, " A systems programming language ment to replace C, yay! ", attr, mkTPos(10, 12));
-    writeString(vram, "Returning from Zig, you should see '", makeColor(TVGAColor.Black, TVGAColor.Green), mkTPos(0, 24));
-    writeString(vram, "OS returned!", makeColor(TVGAColor.Red, TVGAColor.White), mkTPos(36, 24));
-    writeString(vram, "' at top left.", makeColor(TVGAColor.Black, TVGAColor.Green), mkTPos(48, 24));
+
+    vga.init();
+
+    vga.clearScreen(TVGAColor.Green);
+
+    vga.rainbow(" ---=== Booted by GRUB ===--- ", mkTPos(10, 10));
+    vga.writeString(" Hello from Zig ", attr, mkTPos(10, 11));
+    vga.writeString(" A systems programming language ment to replace C, yay! ", attr, mkTPos(10, 12));
+    vga.writeString("Returning from Zig, you should see '", makeColor(TVGAColor.Black, TVGAColor.Cyan), mkTPos(0, 24));
+    vga.writeString("OS returned!", makeColor(TVGAColor.Red, TVGAColor.White), mkTPos(36, 24));
+    vga.writeString("' at top left.", makeColor(TVGAColor.Black, TVGAColor.Cyan), mkTPos(48, 24));
 
     return;
 }
